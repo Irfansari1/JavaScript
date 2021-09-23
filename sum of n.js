784d563789f777abb80cef56d4006eb191295912
Irfansari1 <irfansari42@gmail.com>
2021-09-23 22:16:18 +0200

    sum of n

---
+++ sum of n.js
@@ -0,0 +1,22 @@
+function sum(x){
+    let sum;
+    sum = 0;
+    for (var i = 0; i <= x; i++){
+        sum = sum + i;
+    } 
+    return sum
+    /*if (sum % 2 == 0){
+        return true;
+
+    }
+    else if (sum == 91 ){
+        return "91";
+    }else{
+        return false;
+
+    }*/
+}        
+
+console.log(sum(13));
+
+/* sum of n */

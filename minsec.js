198ffd1d1f3b576c86cf66b48c6047edb954cc54
Irfansari1 <irfansari42@gmail.com>
2021-10-04 00:02:23 +0200

    minsec and session_3

---
+++ minsec.js
@@ -0,0 +1,10 @@
+//Write a function that takes an integer minutes and converts it to seconds.
+
+ 
+ 
+function minToSecond(min){
+    sec = min * 60;
+    return sec;
+};
+
+console.log(minToSecond(3));

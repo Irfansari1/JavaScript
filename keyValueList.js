e70ff53b4e6f8da4c7e1f8cda76eec4c6aa43dca
Irfansari1 <irfansari42@gmail.com>
2021-10-05 23:31:40 +0200

    keyValueList added

---
+++ keyValueList.js
@@ -0,0 +1,19 @@
+//https://edabit.com/challenge/AP4hnF97anRc2mAZ6
+const keyValueList = function(obj){
+
+    let arr1 =[];
+    let arr2=[];
+
+    for (i in obj){
+        arr1.push(i)
+         
+    }
+    arr1.sort();
+    for (var j in arr1){
+        arr2.push(obj[arr1[j]])  //arr1=[ 'a', 'b', 'd' ]  obj["a"] = 3
+    }
+
+    return [arr1,arr2]
+
+
+} 
